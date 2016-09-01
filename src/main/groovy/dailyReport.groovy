086f79ef41f1088cf12969f8879a302b4ac33e71
kriakaziaba <kriakaziaba@gmail.com>
2016-09-01 03:41:05 +0300

    final custom go throe

--- src/main/groovy/dailyReport.groovy
+++ src/main/groovy/dailyReport.groovy
@@ -1,6 +1,8 @@
 /**
  * Created by DiR on 31.08.2016.
  */
+
+import groovyx.net.http.HttpResponseDecorator
 import groovyx.net.http.RESTClient
 import oauth.signpost.*
 import oauth.signpost.basic.*
@@ -15,18 +17,20 @@ def keySecret = 'c4e7f84aafef2d19292268c391fe70aaf84ddfc390d523b9c43e0e89e01ca4f
 def requestTokenUrl = 'https://trello.com/1/OAuthGetRequestToken'
 def authorizeUrl = 'https://trello.com/1/OAuthAuthorizeToken'
 def accessTokenUrl = 'https://trello.com/1/OAuthGetAccessToken'
+def pin = '1c82ab667d1421b22b21711a22779b94'
 
 consumer = new DefaultOAuthConsumer(key, keySecret)
 provider = new DefaultOAuthProvider(
         requestTokenUrl,
         accessTokenUrl,
         authorizeUrl);
-println provider.retrieveRequestToken(consumer, OAuth.OUT_OF_BAND, 'scope', 'read,write', 'name', 'groovytrelloxuy' )
+println provider.retrieveRequestToken(consumer, OAuth.OUT_OF_BAND) + '&scope=read,write&expiration=30days&name=groovytrelloxuy'
 // copy the above line, paste it in your browser; Twitter will ask you to authorize...
-provider.retrieveAccessToken(consumer, '<PIN NUMBER FROM BROWSER>')
+provider.retrieveAccessToken(consumer, pin)
 println consumer.token //<YOUR ACCESS TOKEN>
 println consumer.tokenSecret //<YOUR SECRET TOKEN>
 
 def client = new RESTClient( 'https://api.trello.com/' )
 client.auth.oauth key, keySecret, consumer.token, consumer.tokenSecret
-assert client.get( path : '1/members/me/boards' ).status == 200
+HttpResponseDecorator lol = client.get( path : '1/members/me/boards' )
+assert lol.status == 200
